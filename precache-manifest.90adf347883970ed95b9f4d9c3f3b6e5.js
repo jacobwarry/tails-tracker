b641dacc71ee9df1e6820f1d0f3f0678a7835d9c
unknown <jacob.warry@gmail.com>
2019-09-29 20:10:55 +0200

    Updates

---
+++ precache-manifest.90adf347883970ed95b9f4d9c3f3b6e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edeba0804e8b422ec297105102a10934",
+    "url": "/wondrous-tails/index.html"
+  },
+  {
+    "revision": "026eeb61340c4887bfc4",
+    "url": "/wondrous-tails/static/css/main.4287a31d.chunk.css"
+  },
+  {
+    "revision": "8ab4f1b16e1e5f25c6b5",
+    "url": "/wondrous-tails/static/js/2.b1b08a39.chunk.js"
+  },
+  {
+    "revision": "026eeb61340c4887bfc4",
+    "url": "/wondrous-tails/static/js/main.c2d41051.chunk.js"
+  },
+  {
+    "revision": "e11a9abf13988dc0a35c",
+    "url": "/wondrous-tails/static/js/runtime-main.cab3de07.js"
+  }
+]);
