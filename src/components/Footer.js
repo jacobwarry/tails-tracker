a44f1b9ab3c551e040e41c28139bf219b39f8af2
Jacob Warry <jwarry@facilisgroup.com>
2020-08-16 18:46:53 +0200

    adding moment. setting footer year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import Moment from 'moment';
 
 const Footer = () => {
     return (
         <footer>
-            <div>
-                FINAL FANTASY XIV © 2010 - 2019 SQUARE ENIX CO., LTD. All Rights Reserved.
-            </div>
+            <div>FINAL FANTASY XIV © 2010 - {moment().format('YYYY')} SQUARE ENIX CO., LTD. All Rights Reserved.</div>
         </footer>
     )
 }
